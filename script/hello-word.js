d3eb1efe9a563ddd68571db0c6489f19ed1596aa
Usernamenuh <muhammadenuh4@gmail.com>
2025-08-21 12:43:52 +0700

    Day 1

---
+++ script/hello-word.js
@@ -0,0 +1,42 @@
+document.writeln("Hello World!");
+// Tipe Data Number
+document.writeln(10);
+document.writeln("</br>");
+document.writeln(10.5);
+document.writeln("</br>");
+
+// Number Object hexadecimal, octal, and binary
+document.writeln(0b11111010); // hexadecimal
+document.writeln("</br>");
+document.writeln(0o12); // octal
+document.writeln("</br>");
+document.writeln(0xA); // binary
+document.writeln("</br>");
+
+// Tipe Data Boolean
+document.writeln(true);
+document.writeln("</br>");
+document.writeln(false);
+document.writeln("</br>");
+
+// Tipe Data String
+document.writeln("Muhammad Enuh");
+document.writeln("</br>");
+document.writeln('Hello Nuh!')
+document.writeln("</br>");
+
+// Gabungan String
+document.writeln("Name: " + "Muhammad Enuh");
+document.writeln("</br>");
+document.writeln("Age: " + 20);
+document.writeln("</br>");
+document.writeln("GitHub: " + "https://github.com/usernamenuh");
+document.writeln("</br>");
+
+// Escape Sequence
+document.writeln("<textarea cols='100' rows='10'>");
+document.write("Hello World!\nThis is a new line.\n");
+document.write("\\Muhammad Enuh\n");
+document.write("Hello \"Nuh\"!\n");
+document.write('\'Kematan\'')
+document.writeln("</textarea>");
